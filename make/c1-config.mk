74f19970116f059a373b5be185d22ffa093d22f3
jienfak <jienfak@protonmail.com>
2019-07-09 13:43:50 +0500

    ...

---
+++ make/c1-config.mk
@@ -0,0 +1,23 @@
+# Program name.
+TARGET  =
+# Version
+VERSION =
+
+# Customize below to fit your system and program.
+
+# Paths.
+PREFIX    =
+MANPREFIX = ${PREFIX}/share/man
+
+# Includes.
+INC =
+# Dynamic libraries.
+LIB =
+
+# Compilation Flags.
+CFLAGS   =
+LDFLAGS  = ${LIBS}
+
+# Compiler and linker.
+CC =
+LD = $(CC)
